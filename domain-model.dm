198fe52fe2f2988e15622879a618a25563fe77f6
Ning Ma <ningma905@gmail.com>
2022-06-16 15:13:13 +0100

    set up

---
+++ domain-model.dm
@@ -0,0 +1,30 @@
+class BankAccount
+properties:
+    transactions: []
+
+make withdraw 
+has functions
+balance? and history
+
+
+
+
+class Transaction
+//deposit or withdraw methods
+deposit(amount)
+
+withdraw(amount)
+
+recordTransaction() {
+
+}
+
+stores data
+date
+amount
+type
+
+
+class Statement 
+//generate a statement
+produce a Statement
--- src/Bank.js
+++ src/Bank.js
@@ -1,3 +1,74 @@
 class Bank {
+  constructor() {
+    this.user = new User();
+  }
+
+  getBalance() {
+    return this.user.getBalance();
+  }
+
+  getTransactions() {
+    return this.user.getTransactions();
+  }
+
+  deposit(amount) {
+    this.user.deposit(amount);
+  }
+
+  withdraw(amount) {
+    return this.user.withdraw(amount);
+  }
+}
+
+class User {
+  #balance;
+  #transactions;
+  constructor() {
+    this.#balance = 0;
+    this.#transactions = [];
+  }
+
+  getBalance() {
+    return this.#balance;
+  }
+
+  getTransactions() {
+    return this.#transactions;
+  }
+
+  deposit(amount, date = new Date()) {
+    this.#balance += amount;
+    const transaction = {
+      date: date,
+      credit: "",
+      debit: amount,
+      balance: this.#balance,
+    };
+    this.#transactions.push(transaction);
+  }
+
+  withdraw(amount) {
+    if (this.#balance >= amount) {
+      this.#balance -= amount;
+      return true;
+    }
+    return false;
+  }
+}
+
+class BankAccount {
+  #balance;
+  constructor() {
+    this.#balance = 0;
+  }
+}
+
+class Transaction {
   constructor() {}
+}
+
+class Statement {
+  constructor() {
+    this.statements = [];
+  }
 }
