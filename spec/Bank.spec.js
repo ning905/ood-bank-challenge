2bdec6db64ca11ea94ee9d23ef5e6499258cdd20
Ning Ma <ningma905@gmail.com>
2022-06-16 12:49:24 +0100

    set up

---
+++ spec/Bank.spec.js
@@ -0,0 +1,44 @@
+const Bank = require("../src/Bank");
+
+describe("Bank", () => {
+  let bank;
+
+  beforeEach(() => {
+    bank = new Bank();
+  });
+
+  it("", () => {
+    //set up
+    //execute
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("", () => {
+    //set up
+    //execute
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("", () => {
+    //set up
+    //execute
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("", () => {
+    //set up
+    //execute
+    //verify
+    expect(expected).toEqual(result);
+  });
+
+  it("", () => {
+    //set up
+    //execute
+    //verify
+    expect(expected).toEqual(result);
+  });
+});
